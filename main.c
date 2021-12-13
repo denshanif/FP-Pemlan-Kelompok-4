d08ced2ac6a5a481b125c59d15ca59ea6436e4d2
Hanif <alfatoni922@gmail.com>
2021-12-13 18:09:36 +0700

    Update pencarian barang (linear)

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ barang data['0'];
 FILE *file_data;
 
 void tampilBarang();
+void cariBarang();
 
 int main()
 {
@@ -41,7 +42,8 @@ int main()
 
         printf("\n=== SILAHKAN PILIH MENU YANG DI BAWAH INI ===\n");
         printf("1. MENAMPILKAN SEMUA DATA BARANG.\n");
-        printf("2. KELUAR PROGRAM.\n");
+        printf("2. PENCARIAN DATA BARANG.\n");
+        printf("3. KELUAR PROGRAM.\n");
 
         printf("Masukkan pilihan anda: ");
         scanf("%d", &menu);
@@ -56,6 +58,10 @@ int main()
             printf("\n");
             break;
         case 2:
+            cariBarang();
+            printf("\n");
+            break;
+        case 3:
             exit(0);
         default:
             printf("Maaf, input yang anda masukkan salah/invalid.");
@@ -289,6 +295,138 @@ up:
         goto up;
     case 2:
         tampilHarga();
+        printf("\n");
+        printf("Tekan enter untuk kembali ke menu sebelumnya.\n");
+        getch();
+        goto up;
+    case 3:
+        break;
+    default:
+        printf("Maaf, input yang anda masukkan salah/invalid.");
+        printf("\n");
+        printf("Tekan enter untuk kembali ke menu sebelumnya.\n");
+        getch();
+        goto up;
+    }
+}
+
+void urutKodeAscen()
+{
+    int i, j;
+
+	// Looping bubble sort
+	for (i = z - 1; i > 0; i--) {
+		for (j = z - 1; j >= z - i; j--) {
+			if (data[j].kode < data[j-1].kode) {
+				tukar = data[j];
+				data[j] = data[j-1];
+				data[j-1] = tukar;
+			}
+		}
+	}
+}
+
+void cariKode() {
+    urutKodeAscen();
+
+    int i, flag = 0;
+    int cari;
+
+up:
+	printf("Masukkan kode barang yang ingin dicari: ");
+	scanf("%d", &cari);
+
+	for(i = 0; i < z; i++) {
+		if (data[i].kode == cari) {
+            flag = 1;
+
+            break;
+        }
+	}
+
+	if (flag == 1)
+    {
+        printf("Data barang ditemukan!\n\n");
+
+        printf("Kode Barang : %d\n\n", data[i].kode);
+
+        printf("Kategori Barang : %s\n", data[i].kategori);
+        printf("Nama Barang : %s\n", data[i].nama);
+        printf("Harga Barang : %d\n\n", data[i].harga);
+    }
+    else
+    {
+        printf("Data yang anda cari tidak ditemukan \n");
+        getch();
+        goto up;
+    }
+}
+
+void cariNama() {
+    urutNamaAscen();
+
+    int i, flag = 0;
+    char cari[25];
+
+up:
+	printf("Masukkan nama barang yang ingin dicari: ");
+	scanf("%s", &cari);
+
+	for(i = 0; i < z; i++) {
+		if (strcmp(data[i].nama, cari) == 0) {
+            flag = 1;
+
+            break;
+        }
+	}
+
+	if (flag == 1)
+    {
+        printf("Data barang ditemukan!\n\n");
+
+        printf("Nama Barang : %s\n\n", data[i].nama);
+
+        printf("Kode Barang : %d\n", data[i].kode);
+        printf("Kategori Barang : %s\n", data[i].kategori);
+        printf("Harga Barang : %d\n\n", data[i].harga);
+    }
+    else
+    {
+        printf("Data yang anda cari tidak ditemukan \n");
+        getch();
+        goto up;
+    }
+}
+
+void cariBarang()
+{
+    int menu;
+
+up:
+    system("cls");
+    printf("============================ Pencarian Data Barang ==============================\n\n");
+    printf("\n======================= SILAHKAN PILIH OPSI DI BAWAH INI ======================\n");
+
+    printf("1. CARI DATA BARANG BERDASARKAN KODE BARANG.\n");
+    printf("2. CARI DATA BARANG BERDASARKAN NAMA BARANG.\n");
+    printf("3. KEMBALI KE MENU UTAMA.\n");
+
+    printf("Masukkan pilihan anda: ");
+    scanf("%d", &menu);
+
+    printf("\n");
+
+    // Switch case pemilihan menu
+    switch (menu)
+    {
+    case 1:
+        cariKode();
+        printf("\n");
+        printf("Tekan enter untuk kembali ke menu sebelumnya.\n");
+        getch();
+        goto up;
+    case 2:
+        cariNama();
         printf("\n");
         printf("Tekan enter untuk kembali ke menu sebelumnya.\n");
         getch();
