2f6f5efac3fda9d0a2b33aee149de97d48f50dd1
Ridwan Efendi <ridwan.efen190301@gmail.com>
2021-12-14 10:53:38 +0700

    Perbaikan total harga

--- main.c
+++ main.c
@@ -516,7 +516,7 @@ void pembayaran()
 
     kembalian = uang - totalHarga;
 
-    if (uang > totalHarga) {
+    if (uang >= totalHarga) {
         system("cls");
         printf("===================================================\n");
         printf("        ========== NOTA BELANJA ==========         \n");
@@ -561,10 +561,12 @@ void konfirmasi()
         {
         case 1:
             pembayaran();
+            totalHarga = 0;
             getch();
             break;
         case 2:
             printf("\n");
+            totalHarga = 0;
             break;
         }
     }
